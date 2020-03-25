838eb481f5bb8b958616168e1acd76be36a508e5
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-03-25 12:16:38 -0400

    update

---
+++ BackTracking/CombinationSumII.java
@@ -0,0 +1,60 @@
+/*
+
+40. Combination Sum II
+
+https://leetcode.com/problems/combination-sum-ii/
+
+Given a collection of candidate numbers (candidates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+Each number in candidates may only be used once in the combination.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [10,1,2,7,6,1,5], target = 8,
+A solution set is:
+[
+  [1, 7],
+  [1, 2, 5],
+  [2, 6],
+  [1, 1, 6]
+]
+Example 2:
+
+Input: candidates = [2,5,2,1,2], target = 5,
+A solution set is:
+[
+  [1,2,2],
+  [5]
+]
+
+
+*/
+
+class CombinationSumII {
+    // Back tracking
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> list = new ArrayList<>();
+        Arrays.sort(candidates);
+        backtrack(list, new ArrayList<>(), candidates, target, 0);
+        return list;
+    }
+    
+    private void backtrack(List<List<Integer>> list, List<Integer> tempList, int[] candidates, int remain, int start){
+        if (remain < 0 || list.contains(tempList)) return;
+        else if (remain == 0) list.add(new ArrayList<>(tempList));
+        else {
+            if(i > start && candidates[i] == candidates[i-1]) continue; // skip duplicates
+            for (int i = start; i < candidates.length; i++){
+                tempList.add(candidates[i]);
+                backtrack(list, tempList, candidates, remain - candidates[i], i+1);
+                tempList.remove(tempList.size()-1);
+            }
+        }
+    }
+
+
+}
