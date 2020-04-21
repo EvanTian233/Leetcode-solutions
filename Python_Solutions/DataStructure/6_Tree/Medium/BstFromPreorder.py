a32e332deedd7c0ed037bd0578300c12f29e670c
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-04-20 22:26:40 -0400

    update

---
+++ Python_Solutions/DataStructure/6_Tree/Medium/BstFromPreorder.py
@@ -0,0 +1,41 @@
+"""
+1008. Construct Binary Search Tree from Preorder Traversal
+https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+Return the root node of a binary search tree that matches the given preorder traversal.
+
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+ 
+
+Example 1:
+
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+
+
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def bstFromPreorder(self, preorder: List[int]) -> TreeNode:
+        root = TreeNode(preorder[0])
+        stack = [root]
+        for value in preorder[1:]:
+            if value < stack[-1].val:
+                stack[-1].left = TreeNode(value)
+                stack.append(stack[-1].left)
+            else:
+                while stack and stack[-1].val < value:
+                    last = stack.pop()
+                last.right = TreeNode(value)
+                stack.append(last.right)
+        return root
