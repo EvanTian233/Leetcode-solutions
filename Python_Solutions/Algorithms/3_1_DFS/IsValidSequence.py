d5a76a2f7e118a3d6d4170d0d7b81962058e8fe4
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-04-30 11:42:05 -0400

    update

---
+++ Python_Solutions/Algorithms/3_1_DFS/IsValidSequence.py
@@ -0,0 +1,39 @@
+"""
+Check If a String Is a Valid Sequence from Root to Leaves Path in a Binary Tree
+
+Given a binary tree where each path going from the root to any leaf form a valid sequence, check if a given string is a valid sequence in such binary tree. 
+
+We get the given string from the concatenation of an array of integers arr and the concatenation of all values of the nodes along a path results in a sequence in the given binary tree.
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,1,0,1]
+Output: true
+Explanation: 
+The path 0 -> 1 -> 0 -> 1 is a valid sequence (green color in the figure). 
+Other valid sequences are: 
+0 -> 1 -> 1 -> 0 
+0 -> 0 -> 0
+
+Input: root = [0,1,0,0,1,0,null,null,1,0,0], arr = [0,0,1]
+Output: false 
+Explanation: The path 0 -> 0 -> 1 does not exist, therefore it is not even a sequence.
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isValidSequence(self, root: TreeNode, arr: List[int]) -> bool:
+        def dfs(root, pos):
+            if not root or root.val != arr[pos]:
+                return False
+            if pos == len(arr) - 1:
+                return not root.left and not root.right
+            return dfs(root.left, pos + 1) or dfs(root.right, pos + 1)
+        
+        return dfs(root, 0)
+        
+        
