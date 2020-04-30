57cba49d05b9582df39be44e396b83babbb779b2
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-04-29 23:49:04 -0400

    update

---
+++ Python_Solutions/Algorithms/3_1_DFS/Hard/MaxPathSum.py
@@ -0,0 +1,59 @@
+"""
+124. Binary Tree Maximum Path Sum
+https://leetcode.com/problems/binary-tree-maximum-path-sum/
+
+Given a non-empty binary tree, find the maximum path sum.
+
+For this problem, a path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections. The path must contain at least one node and does not need to go through the root.
+
+Example 1:
+
+Input: [1,2,3]
+
+       1
+      / \
+     2   3
+
+Output: 6
+Example 2:
+
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def maxPathSum(self, root: TreeNode) -> int:
+        self.max_sum = float('-inf')
+        self.dfs(root)
+        return self.max_sum
+    
+    def dfs(self, node):
+        if not node: return 0
+        
+        # only add positive contributions
+        leftST_sum = max(0, self.dfs(node.left))
+        rightST_sum = max(0, self.dfs(node.right))
+
+        # check if cumulative sum at current node > global max sum so far
+        # this evaluates a candidate path
+        self.max_sum = max(self.max_sum, leftST_sum + rightST_sum + node.val)
+        
+        # add to the current node ONLY one of the children contributions
+        # in order to maintain the constraint of considering only paths
+        # if not, we would be exploring explore the whole tree - against problem definition
+        return max(leftST_sum, rightST_sum) + node.val
