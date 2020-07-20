5d74368ead6549f6ebfe5bf125ad927072478c10
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-07-19 23:04:08 -0400

    update

---
+++ Python_Solutions/Algorithms/7_GraphTheory/FindOrder.py
@@ -0,0 +1,49 @@
+"""
+210. Course Schedule II
+https://leetcode.com/problems/course-schedule-ii/
+
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+
+Input: 2, [[1,0]] 
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished   
+             course 0. So the correct course order is [0,1] .
+Example 2:
+
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both     
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0. 
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+
+
+"""
+
+## Topological sort
+
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        dic = collections.defaultdict(set)
+        neigh = collections.defaultdict(set)
+        for i, j in prerequisites:
+            dic[i].add(j)
+            neigh[j].add(i)
+        stack = [i for i in range(numCourses) if not dic[i]]
+        res = []
+        while stack:
+            node = stack.pop()
+            res.append(node)
+            for i in neigh[node]:
+                dic[i].remove(node)
+                if not dic[i]:
+                    stack.append(i)
+            dic.pop(node)
+        return res if not dic else []
