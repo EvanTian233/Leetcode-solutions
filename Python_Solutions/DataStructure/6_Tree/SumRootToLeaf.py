558b398c8b3e912f9bc928e46a6566752b2f8a83
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-09-08 11:33:20 -0400

    update

---
+++ Python_Solutions/DataStructure/6_Tree/SumRootToLeaf.py
@@ -0,0 +1,32 @@
+"""
+1022. Sum of Root To Leaf Binary Numbers
+https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+
+Given a binary tree, each node has value 0 or 1.  Each root-to-leaf path represents a binary number starting with the most significant bit.  For example, if the path is 0 -> 1 -> 1 -> 0 -> 1, then this could represent 01101 in binary, which is 13.
+
+For all leaves in the tree, consider the numbers represented by the path from the root to that leaf.
+
+Return the sum of these numbers.
+
+
+Example 1:
+
+Input: [1,0,1,0,1,0,1]
+Output: 22
+Explanation: (100) + (101) + (110) + (111) = 4 + 5 + 6 + 7 = 22
+
+"""
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def sumRootToLeaf(self, root, val=0):
+        if not root: return 0
+        val = val * 2 + root.val
+        if root.left == root.right: return val
+        return self.sumRootToLeaf(root.left, val) + self.sumRootToLeaf(root.right, val)
+        
