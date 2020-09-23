52fe1ff5c4f62f05ed22c9d089686cc64a539ee8
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-09-22 22:31:19 -0400

    update

---
+++ Python_Solutions/DataStructure/3_HashTable/MajorityElement.py
@@ -0,0 +1,29 @@
+"""
+229. Majority Element II
+https://leetcode.com/problems/majority-element-ii/Given 
+
+an integer array of size n, find all elements that appear more than ⌊ n/3 ⌋ times.
+
+Note: The algorithm should run in linear time and in O(1) space.
+
+Example 1:
+
+Input: [3,2,3]
+Output: [3]
+Example 2:
+
+Input: [1,1,1,3,3,2,2,2]
+Output: [1,2]
+
+"""
+
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> List[int]:
+        
+        ctr = collections.Counter()
+        for n in nums:
+            ctr[n] += 1
+            if len(ctr) == 3:
+                ctr -= collections.Counter(set(ctr))
+        return [n for n in ctr if nums.count(n) > len(nums)/3]
