52c579d2425af59d3181108937a7817fbdade379
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-10-09 15:10:47 -0400

    update

---
+++ Python_Solutions/DataStructure/6_Tree/SerializeDeserialize.py
@@ -0,0 +1,64 @@
+"""
+449. Serialize and Deserialize BST
+https://leetcode.com/problems/serialize-and-deserialize-bst/
+
+Serialization is converting a data structure or object into a sequence of bits so that it can be stored in a file or memory buffer, or transmitted across a network connection link to be reconstructed later in the same or another computer environment.
+
+Design an algorithm to serialize and deserialize a binary search tree. There is no restriction on how your serialization/deserialization algorithm should work. You need to ensure that a binary search tree can be serialized to a string, and this string can be deserialized to the original tree structure.
+
+The encoded string should be as compact as possible.
+
+ 
+
+Example 1:
+
+Input: root = [2,1,3]
+Output: [2,1,3]
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        vals = []
+
+        def preOrder(node):
+            if node:
+                vals.append(node.val)
+                preOrder(node.left)
+                preOrder(node.right)
+
+        preOrder(root)
+
+        return ' '.join(map(str, vals))
+
+    # O( N ) since each val run build once
+    def deserialize(self, data):
+        vals = collections.deque(int(val) for val in data.split())
+
+        def build(minVal, maxVal):
+            if vals and minVal < vals[0] < maxVal:
+                val = vals.popleft()
+                node = TreeNode(val)
+                node.left = build(minVal, val)
+                node.right = build(val, maxVal)
+                return node
+
+        return build(float('-infinity'), float('infinity'))
+
+    
+# Your Codec object will be instantiated and called as such:
+# Your Codec object will be instantiated and called as such:
+# ser = Codec()
+# deser = Codec()
+# tree = ser.serialize(root)
+# ans = deser.deserialize(tree)
+# return ans
