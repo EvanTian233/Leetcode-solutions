5db1226bb13c21a682026b919f2e8ff4fc287c8c
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-10-11 12:59:48 -0400

    update

---
+++ Python_Solutions/Algorithms/4_DynamicProgramming/RemoveDuplicateLetters.py
@@ -0,0 +1,27 @@
+"""
+316. Remove Duplicate Letters
+https://leetcode.com/problems/remove-duplicate-letters/
+
+Given a string s, remove duplicate letters so that every letter appears once and only once. You must make sure your result is the smallest in lexicographical order among all possible results.
+
+Note: This question is the same as 1081: https://leetcode.com/problems/smallest-subsequence-of-distinct-characters/
+
+ 
+
+Example 1:
+
+Input: s = "bcabc"
+Output: "abc"
+
+"""
+
+class Solution:
+    def removeDuplicateLetters(self, s):
+        result = ''
+        while s:
+            i = min(map(s.rindex, set(s)))
+            c = min(s[:i+1])
+            result += c
+            s = s[s.index(c):].replace(c,'')
+
+        return result
