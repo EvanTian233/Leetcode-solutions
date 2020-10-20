e1a3d1a55055f79194736fa19c1b027022e95f06
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-10-19 22:13:28 -0400

    update

---
+++ Python_Solutions/Algorithms/4_DynamicProgramming/MinDominoRotations.py
@@ -0,0 +1,48 @@
+"""
+1007. Minimum Domino Rotations For Equal Row
+https://leetcode.com/problems/minimum-domino-rotations-for-equal-row/
+
+In a row of dominoes, A[i] and B[i] represent the top and bottom halves of the ith domino.  (A domino is a tile with two numbers from 1 to 6 - one on each half of the tile.)
+
+We may rotate the ith domino, so that A[i] and B[i] swap values.
+
+Return the minimum number of rotations so that all the values in A are the same, or all the values in B are the same.
+
+If it cannot be done, return -1.
+
+ 
+
+Example 1:
+
+
+Input: A = [2,1,2,4,2,2], B = [5,2,6,2,3,2]
+Output: 2
+Explanation: 
+The first figure represents the dominoes as given by A and B: before we do any rotations.
+If we rotate the second and fourth dominoes, we can make every value in the top row equal to 2, as indicated by the second figure.
+
+"""
+
+
+class Solution:
+    def minDominoRotations(self, A: List[int], B: List[int]) -> int:
+        n = len(A)
+        cntA = [0] * 7
+        cntB = [0] * 7
+        cntSame = [0] * 7
+        
+        for i in range(n):
+            a, b = A[i], B[i]
+            cntA[a] += 1
+            cntB[b] += 1
+            if a == b: cntSame[a] += 1
+        
+        ans = n
+        for v in range(1, 7):
+            if cntA[v] + cntB[v] - cntSame[v] == n:
+                minSwap = min(cntA[v], cntB[v]) - cntSame[v]
+                ans = min(ans, minSwap)
+            
+        return -1 if ans == n else ans
+            
+        
