3dd378b7e5339abe5af8df9cbfc384d38b592bc6
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-10-20 21:59:28 -0400

    update

---
+++ Python_Solutions/DataStructure/7_Graph/CloneGraph.py
@@ -0,0 +1,40 @@
+"""
+133. Clone Graph
+https://leetcode.com/problems/clone-graph/
+
+Given a reference of a node in a connected undirected graph.
+
+Return a deep copy (clone) of the graph.
+
+Each node in the graph contains a val (int) and a list (List[Node]) of its neighbors.
+
+
+
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: 'Node') -> 'Node':
+        if not node:
+            return
+        
+        nodeCopy = Node(node.val, [])
+        dic = {node: nodeCopy}
+        queue = collections.deque([node])
+        while queue:
+            node = queue.popleft()
+            for neighbor in node.neighbors:
+                if neighbor not in dic:
+                    neighborCopy = Node(neighbor.val, [])
+                    dic[neighbor] = neighborCopy
+                    dic[node].neighbors.append(neighborCopy)
+                    queue.append(neighbor)
+                else:
+                    dic[node].neighbors.append(dic[neighbor])
+        return nodeCopy
+
+
