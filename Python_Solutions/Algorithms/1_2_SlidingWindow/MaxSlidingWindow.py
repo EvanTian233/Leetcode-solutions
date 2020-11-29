7546e68773dc303fce3b04cbd8f56d61fb7bacbd
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2020-11-29 00:04:35 -0500

    update

---
+++ Python_Solutions/Algorithms/1_2_SlidingWindow/MaxSlidingWindow.py
@@ -0,0 +1,70 @@
+"""
+239. Sliding Window Maximum
+https://leetcode.com/problems/sliding-window-maximum/
+
+You are given an array of integers nums, there is a sliding window of size k which is moving from the very left of the array to the very right. You can only see the k numbers in the window. Each time the sliding window moves right by one position.
+
+Return the max sliding window.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,-1,-3,5,3,6,7], k = 3
+Output: [3,3,5,5,6,7]
+Explanation: 
+Window position                Max
+---------------               -----
+[1  3  -1] -3  5  3  6  7       3
+ 1 [3  -1  -3] 5  3  6  7       3
+ 1  3 [-1  -3  5] 3  6  7       5
+ 1  3  -1 [-3  5  3] 6  7       5
+ 1  3  -1  -3 [5  3  6] 7       6
+ 1  3  -1  -3  5 [3  6  7]      7
+"""
+
+from collections import deque
+class Solution:
+    def maxSlidingWindow(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        if not nums:
+            return []
+        if k == 0:
+            return nums
+        
+        # Defining Deque and result list
+        deq = deque()
+        result = []
+        
+
+        for i in range(k):
+            while len(deq) != 0:
+                if nums[i] > nums[deq[-1]]:
+                    deq.pop()
+                else:
+                    break
+
+            deq.append(i)
+            
+ 
+        for i in range(k, len(nums)):
+            result.append(nums[deq[0]])
+            
+            if deq[0] < i - k + 1:
+                deq.popleft()
+            
+            while len(deq) != 0:
+                if nums[i] > nums[deq[-1]]:
+                    deq.pop()
+                else:
+                    break
+            
+            deq.append(i)
+        
+        result.append(nums[deq[0]])
+        
+        return result
