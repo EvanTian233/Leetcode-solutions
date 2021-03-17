715d8613006b6a24b88c0d95a1487eec94e31c7d
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2021-03-16 23:09:10 -0400

    update

---
+++ Python_Solutions/Algorithms/6_Greedy/MaxProfit.py
@@ -0,0 +1,39 @@
+"""
+714. Best Time to Buy and Sell Stock with Transaction Fee
+https://leetcode.com/problems/best-time-to-buy-and-sell-stock-with-transaction-fee/
+
+You are given an array prices where prices[i] is the price of a given stock on the ith day, and an integer fee representing a transaction fee.
+
+Find the maximum profit you can achieve. You may complete as many transactions as you like, but you need to pay the transaction fee for each transaction.
+
+Note: You may not engage in multiple transactions simultaneously (i.e., you must sell the stock before you buy again).
+
+ 
+
+Example 1:
+
+Input: prices = [1,3,2,8,4,9], fee = 2
+Output: 8
+Explanation: The maximum profit can be achieved by:
+- Buying at prices[0] = 1
+- Selling at prices[3] = 8
+- Buying at prices[4] = 4
+- Selling at prices[5] = 9
+The total profit is ((8 - 1) - 2) + ((9 - 4) - 2) = 8.
+
+"""
+
+class Solution:
+    def maxProfit(self, prices: List[int], fee: int) -> int:
+        n = len(prices)
+        if n < 2:
+             return 0
+        ans = 0
+        minimum = prices[0]
+        for i in range(1, n):
+            if prices[i] < minimum:
+                minimum = prices[i]
+            elif prices[i] > minimum + fee:
+                ans += prices[i] - fee - minimum
+                minimum = prices[i] - fee
+        return ans
