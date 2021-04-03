7a705e59fae3cf8d0d1d414d2771c764a26efd4c
EvanTian <36639992+EvanTian233@users.noreply.github.com>
2021-04-03 15:10:27 -0400

    update

---
+++ Python_Solutions/DataStructure/5_Stacks&Queues/LongestValidParentheses.py
@@ -0,0 +1,40 @@
+"""
+32. Longest Valid Parentheses
+https://leetcode.com/problems/longest-valid-parentheses/
+
+Given a string containing just the characters '(' and ')', find the length of the longest valid (well-formed) parentheses substring.
+
+Example 1:
+
+Input: s = "(()"
+Output: 2
+Explanation: The longest valid parentheses substring is "()".
+Example 2:
+
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parentheses substring is "()()".
+Example 3:
+
+Input: s = ""
+Output: 0
+
+"""
+
+class Solution:
+    def longestValidParentheses(self, s: str) -> int:
+        stack = [0]
+        longest = 0
+        
+        for c in s:
+            if c == "(":
+                stack.append(0)
+            else:
+                if len(stack) > 1:
+                    val = stack.pop()
+                    stack[-1] += val + 2
+                    longest = max(longest, stack[-1])
+                else:
+                    stack = [0]
+
+        return longest
